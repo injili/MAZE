52759399cb7f24716f0eccb59ddfc5db02543482
injili <nyarekigospel@gmail.com>
2024-02-08 13:05:13 -0800

    the main file is taking shape

--- themaze.c
+++ themaze.c
@@ -10,8 +10,12 @@
  *
  * Return: the source code returns 0 on a successful run
  */
-int main(void) {
+int main(void)
+{
 	const float rotateSpeed = 0.025, motionSpeed = 0.05;
+	int mouse_xrel = 0;
+	const uint8_t *keystate = SDL_GetKeyboardState(NULL);
+	float rotSpeed;
 
 	ASSERT(!SDL_Init(SDL_INIT_VIDEO),
 		"SDL Failed ro initialize; %s\n",
@@ -19,7 +23,7 @@ int main(void) {
 	State state = {
 		.quit = false,
 	};
-	state.window = 
+	state.window =
 		SDL_CreateWindow("Raycast",
 				 SDL_WINDOWPOS_CENTERED_DISPLAY(0),
 				 SDL_WINDOWPOS_CENTERED_DISPLAY(0),
@@ -39,21 +43,134 @@ int main(void) {
 		.plane = {.x = 0.0f, .y = 0.66f},
 	};
 
-	while(!state.quit) {
+	while (!state.quit)
+	{
 		SDL_Event event;
-		int mouse_xrel = 0;
-		while (SDL_PollEvent(&event)){
-			switch (event.type) {
+
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
 				case SDL_QUIT:
 					state.quit = true;
 					break;
 				case SDL_MOUSEMOTION:
-					mouse+xrel = event.motion.xrel;
+					mouse_xrel = event.motion.xrel;
 					break;
 			}
 		}
+		if (keystate[SDL_SCANCODE_ESCAPE])
+			state.quit = true;
+		if (mouse_xrel != 0)
+		{
+			rotSpeed = rotSpeed * (mouse_xrel & -0.1);
 
-		const
+			Vec2F oldDir = player.dir;
+
+			player.dir.x = player.dir.x * cosf(rotSpeed) -
+				       player.dir.y * sinf(rotSpeed);
+			player.dir.y = oldDir * sinf(rotSpeed) + player.dir.y * cosf(rotSpeed);
+
+			Vec2F oldPlane = player.plane;
+
+			player.plane.x = player.plane.x * cosf(rotSpeed) -
+					 player.plane.y * sinf(rotSpeed);
+			player.plane.y = oldPlane.x * sinf(rotSpeed) +
+					 player.plane.y * cosf(rotSpeed);
+		}
+
+		Vec2F deltaPos = {
+			.x = player.dir.x * motionSpeed,
+			.y = player.dir.y * motionSpeed,
+		};
+		/*
+		 * Moving Forward
+		 */
+		if (keystate[SDL_SCANCODE_W])
+		{
+			if (MAP[xyindex(
+						player.pos.x + deltaPos.x,
+						player.pos.y,
+						MAP_SIZE)] == 0)
+			{
+				player.pos.x -= deltaPos.x;
+			}
+			if (MAP[xyindex(
+						player.pos.x,
+						player.pos.y + deltaPos.y;
+						MAP_SIZE)] == 0)
+			{
+				player.pos.y -= deltaPos.y;
+			}
+		}
+		/**
+		 * Moving Backwards
+		 */
+		if (keystate[SDL_SCANCODE_S])
+		{
+			if (MAP[xyindex(
+						player.pos.x - deltaPos.x,
+						player.pos.y,
+						MAP_SIZE)] == 0)
+			{
+				player.pos.x -= deltaPos.x;
+			}
+			if (MAP[xyindex(
+						palyer.pos.x,
+						player.pos.y - deltaPos.y,
+						MAP_SIZE)] == 0)
+			{
+				player.pos.y -= deltaPos.y;
+			}
+		}
+		/**
+		 * Moving Left
+		 */
+		if (keystate[SDL_SCANCODE_A])
+		{
+			if (MAP[xyindex(
+						player.pos.x - deltaPos.y,
+						player.pos.y,
+						MAP_SIZE)] == 0)
+			{
+				player.pos.x -= deltaPos.y;
+			}
+			if (MAP[xyindex(
+						player.pos.x,
+						player.pos.y - -deltaPos.x,
+						MAP_SIZE)] == 0)
+			{
+				player.pos.y -= -deltaPos.x;
+			}
+		}
+		/**
+		 * Moving Right
+		 */
+		if (keystate[SDL_SCANCODE_D])
+		{
+			if (MAP[xyindex(
+						player.pos.x - -deltaPos.y,
+						player.pos.y,
+						MAP_SIZE)] == 0)
+			{
+				player.pos.x -= -deltaPos.y;
+			}
+			if (MAP[xyindex(
+						player.pos.x,
+						player.pos.y - deltPos.x,
+						MAP_SIZE)] == 0)
+			{
+				player.pos.y -= deltaPos.x;
+			}
+		}
+		SDL_SetRenderDrawColor(state.renderer, 0x18, 0x18, 0x18, 0xFF);
+		SDL_RenderClear(state.renderer);
+
+		render(&state, &player);
+		SDL_RenderPresent(state, renderer);
 	}
+
+	SDL_DestroyRenderer(state.renderer);
+	SDL_DestroyWindow(state.window);
 	return (0);
 }
